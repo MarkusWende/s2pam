0baead1cae6a330ea255a8ba378c4af24ed9d8f2
Markus Wende <markuswende@hotmail.com>
2018-08-21 15:50:45 +0200

    commented textgrid.h and textgrid.cpp

--- src/include/textgrid.h
+++ src/include/textgrid.h
@@ -1,3 +1,23 @@
+/**
+ * @file		textgrid.h
+ * @class		TextGrid
+ *
+ * @brief		TextGrid file manipulation
+ *
+ *					This class meant to work with TextGrid files,
+ *					which are part of the TIMIT Speach Corpus.
+ *					This class reads the TextGrid file and can
+ *					print the content of the class structure.
+ *
+ * @note		TextGrid files should all have the same structure
+ *					including empty lines.
+ *
+ * @author	Markus Wende
+ * @version 1.0
+ * @date		2017-2018
+ * @bug			No known bugs.
+ */
+
 #ifndef TEXTGRID_H
 #define TEXTGRID_H
 
@@ -7,55 +27,65 @@
 #include <cctype>
 #include <algorithm>
 #include <string.h>
-#include <thread>										// std::this_thread::sleep_for
-#include <chrono>										// std::chrono::seconds
-#include <stdlib.h>									// malloc, free, rand
+#include <thread>
+#include <chrono>
+#include <stdlib.h>
 
-//-------------------------------------------------------------------------//
-//               TextGrid stuff                                            //
-//-------------------------------------------------------------------------//
-
-typedef	struct INTERVAL_CONTAINER {
-		float xmin;
-		float xmax;
-		std::string text;
+///	Time segment container, stores t min and max and value
+typedef	struct INTERVAL_CONTAINER
+{
+	float xmin;
+	float xmax;
+	std::string text;
 } interval_c;
 
-typedef	struct ITEM_CONTAINER {
-		std::string cla;
-		std::string name;
-		float xmin;
-		float xmax;
-		int size;
+///	Item container, whether its phonetic, vocal/consonant etc
+typedef	struct ITEM_CONTAINER
+{
+	std::string cla;
+	std::string name;
+	float xmin;
+	float xmax;
+	int size;
 
-		std::vector<interval_c> interval;
+	std::vector<interval_c> interval;
 } item_c;
 
-typedef	struct TEXTGRID_CONTAINER {
-		std::string FileType;
-		std::string objectClass;
-		float xmin;
-		float xmax;
-		bool tiers;
-		int size;
+///	Root container
+typedef	struct TEXTGRID_CONTAINER
+{
+	std::string FileType;
+	std::string objectClass;
+	float xmin;
+	float xmax;
+	bool tiers;
+	int size;
 
-		std::vector<item_c> item;
+	std::vector<item_c> item;
 } textgrid_c;
 
-//----------------------------------------------------------------------------
 
-class Textgrid {
+class Textgrid
+{
 
 private:
-    textgrid_c tg;
+	///	internal TextGrid container
+	textgrid_c tg;
 
 public:
-    Textgrid(const char *fname);
-    ~Textgrid();
-		
-		void print_textgrid_struct();
-};
+	/**
+	 * Constructor
+	 * @param	fname contains the path and filename of the TextGrid file
+	 */
+	Textgrid(const char *fname);
 
-//----------------------------------------------------------------------------
-
-#endif
+	/// Destructor
+	~Textgrid();
+	
+	/**	
+	 * print textgrid structure
+	 * @return	void
+	 */
+	void print_textgrid_struct();
+};																											// end of class TextGrid
+#endif		// TEXTGRID_H
